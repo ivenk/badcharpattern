028a7cf62ed2487c11e8ecd5eebe3d45fc7063e1
iven_k <koethke@th-brandenburg.de>
2018-12-14 16:24:12 +0100

    Implemented second part of functionality. The script can now compare a given input pattern against the original one and print out the missing values.

--- bad_char_pattern.py
+++ bad_char_pattern.py
@@ -9,15 +9,20 @@ def run():
     elif (sys.argv[1] == '-h') or (sys.argv[1]=='--help'):
         print buildUsage()
     elif len(sys.argv) == 2:
-        print "Not yet implemented, sorry!"
+        print "The characters found missing: "
+        chars = comparePattern(sys.argv[1]) 
+        if chars == "":
+            chars = "None"
+        print chars
 
+        
 # builds and returns the usage string
 def buildUsage():
     txt = ""
     txt += "Usage: \n"
-    txt += "python badcharpattern.py (< pattern of ascii chars >)\n"
+    txt += "python badcharpattern.py ('<pattern of ascii chars>')\n"
     txt += "    - no argument: prints pattern of ascii(256) chars \n"
-    txt += "    - one argument <pattern of ascii chars>: prints the differences between the supplied pattern and the original one. "
+    txt += "    - one argument <pattern of ascii chars>: prints the differences between the supplied pattern and the original one. Mind the '' "
     return txt
 
 # builds and returns the string of potentially bad characters
@@ -34,4 +39,26 @@ def buildChars():
 
     return pattern
 
+# compare the new pattern to the orignal one and returns the missing chars
+def comparePattern(new_pattern):
+    # the resulting array has 257 chars because the first one is empty
+    orig = buildChars().split('\\')
+    new = new_pattern.split('\\')
+
+    # we make them start at 1 because of the splitting
+    o = 1
+    n = 1
+
+    chars = ""
+    while o < 257:
+        if orig[o] != new[n]:
+            chars += "\\"
+            chars += orig[o] 
+        else:
+            n += 1
+        o += 1
+
+    
+    return chars
+    
 run()
