65b2b63eb046070f6fc1d1d1e3ed508e6dba3970
iven_k <koethke@th-brandenburg.de>
2018-12-14 14:39:40 +0100

    Added template for argument handling.

--- bad_char_pattern.py
+++ bad_char_pattern.py
@@ -1,11 +1,29 @@
-pattern = ''
-c = 0
-while c < 256:
-    # we want \x01 instead of \x1
-    if c < 16:
-        pattern += "\\x0%x" %c
-    else:
-        pattern += "\\x%x" %c 
-    c += 1
+import sys
 
-print pattern
+def run():
+    if len(sys.argv) > 2:
+        print buildUsage()
+    elif len(sys.argv) == 1:
+        print buildChars()
+    elif len(sys.argv) == 2:
+        print "Not yet implemented, sorry!"
+
+def buildUsage():
+
+    return ""
+
+# builds the string of potentially bad characters
+def buildChars():
+    pattern = ''
+    c = 0
+    while c < 256:
+        # we want \x01 instead of \x1
+        if c < 16:
+            pattern += "\\x0%x" %c
+        else:
+            pattern += "\\x%x" %c 
+        c += 1
+
+    return pattern
+
+run()
